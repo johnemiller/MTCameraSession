44bebfe2eec31a6edca3a56113248ea366bac55b
John E Miller <john.turbo911@gmail.com>
2014-08-14 13:51:54 -0700

    Update MTCameraSession.h

--- MTCameraSession.h
+++ MTCameraSession.h
@@ -1,22 +1,22 @@
 //
-//  MTCameraSession.h
-//  Camera Capture Session
+// MTCameraSession.h
+// Camera Capture Session
 //
-//  https://github.com/johnemiller/MTCameraSession
+// https://github.com/johnemiller/MTCameraSession
 //
-//  Created by John Miller on 12/31/12.
+// Created by John Miller on 12/31/12.
 //
-//  Copyright 2013, Miltech Consulting
+// Copyright 2013, Miltech Consulting
 //
-//  This file is part of MTCameraSession. This software may be used and distributed
-//  according to the terms of the GNU General Public License version 2,
-//  incorporated herein by reference.
+// This file is part of MTCameraSession. This software may be used and distributed
+// according to the terms of the GNU General Public License version 2,
+// incorporated herein by reference.
 //
-
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
 
 @protocol MTVideoCanvasP <NSObject>
+
 @required
 
 @property(nonatomic,readonly)AVCaptureVideoPreviewLayer *viewFinder;
@@ -24,14 +24,16 @@
 @end
 
 @interface MTCameraSession : NSObject<AVCaptureVideoDataOutputSampleBufferDelegate>
+
 ///
-/// Preferred Initializer.  Pass the MTVideoCanvasP compliant view which implements
-/// a sublayer that conforms to the MTVideoCanvas protocol.  This would normally
-/// be your "viewfinder" view.
+/// Preferred Initializer. Pass the MTVideoCanvasP compliant view which implements
+/// a sublayer that conforms to the MTVideoCanvas protocol. This would normally
+/// be your "viewfinder" view.  The bitrate specifies the capture quality of the
+/// session and should be specified as a valid AVCaptureSessionPreset constant.
 ///
-- (id)initWithDisplayCanvas:(UIView<MTVideoCanvasP>*)displayCanvas;
+- (id)initWithDisplayCanvas:(UIView<MTVideoCanvasP>*)displayCanvas bitrate:(NSString *)avCaptureSessionPreset;
 ///
-/// Call this method to initiate an image caprture "session".  This will direct the video
+/// Call this method to initiate an image caprture "session". This will direct the video
 /// output of the camera to the specified displayCanvas.viewFinder specified in the
 /// initWithDisplayCanvas method.
 ///
@@ -43,10 +45,11 @@
 ///
 - (void)captureStillJPEG:(void(^)(NSData* imageData, NSError *error))handler;
 ///
-/// Call this method to end a camera capture session.  This method should be called if the capture
-/// session is restarted, aborted or over for any other reason.  All necessary teardown of the
+/// Call this method to end a camera capture session. This method should be called if the capture
+/// session is restarted, aborted or over for any other reason. All necessary teardown of the
 /// camera session is performed here.
 ///
 - (void)stopSession;
 
 @end
+
